64a4ea9503f87c2a8b14b86955c0007e93aa4a6b
tk-dao <tuankhanhdao1370@gmail.com>
2026-05-02 18:00:39 -0700

    ui skeleton

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,1 +1,22 @@
-// FS2: top-level routing + state
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Hero from "./pages/Hero"
+import InputPage from './pages/InputPage'
+import MountainPage from './pages/MountainPage'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Hero />}></Route>
+        <Route path='/start' element={<InputPage />}></Route>
+        <Route path='/mountain' element={<MountainPage />}></Route>
+      </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
